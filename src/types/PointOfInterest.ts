1209307aa46c1c8f1a9a3b2b47d365fa5f1df2a7
Fabian Schmidt <fabian@ds-fs.de>
2021-03-12 14:59:08 +0100

    type & example data

---
+++ src/types/PointOfInterest.ts
@@ -0,0 +1,23 @@
+import type { LatLngExpression } from "leaflet";
+
+
+export interface PointOfInterest {
+  id: number,
+  latlng: LatLngExpression,
+  name: string,
+  description: string,
+  address: string,
+  category: string
+}
+
+interface Map {
+  values: PointOfInterest[],
+  onSelect: (entry : PointOfInterest) => void,
+  selectedEntry : PointOfInterest
+}
+
+interface Sidebar {
+  values: PointOfInterest[],
+  onSelect: (entry : PointOfInterest) => void,
+  selectedEntry : PointOfInterest
+}
