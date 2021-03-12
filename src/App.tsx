f861b050d10df3b5bb8a9c24f9b70c70c60d3984
Moritz Stueckler <moritz@elbstack.com>
2021-03-12 12:03:36 +0100

    chore: remove snowpack boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import type { LatLngExpression } from 'leaflet';
 import { TileLayer, MapContainer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
-interface AppProps {
-}
+interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  const hamburgCenter: LatLngExpression = [
+    53.55035993851227,
+    9.986701679608633,
+  ];
   return (
-      <MapContainer id={"mapid"} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+    <MapContainer
+      id={'mapid'}
+      center={hamburgCenter}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={hamburgCenter}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
