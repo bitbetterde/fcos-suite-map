f570d5ce116e420d0c1130746e822e1fecd842c5
Moritz Stueckler <moritz.stueckler@gmail.com>
2021-05-21 16:25:43 +0200

    feat: add react-select

--- src/components/Sidebar/SidebarListView.tsx
+++ src/components/Sidebar/SidebarListView.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { usePoiData, useStore } from '../../hooks';
 import ListElement from './ListElement';
 import SidebarContainer from './SidebarContainer';
+import Select from 'react-select';
 
 const SidebarListView: React.FC = () => {
   const { data } = usePoiData();
@@ -9,8 +10,24 @@ const SidebarListView: React.FC = () => {
   const setHoveredPoi = useStore((state) => state.setHoveredPoi);
   const setSelectedPoi = useStore((state) => state.setSelectedPoi);
 
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
     <SidebarContainer>
+      <div className="p-4">
+        <Select
+          defaultValue={[options[2], options[3]]}
+          isMulti
+          name="pois"
+          options={options}
+          className="basic-multi-select"
+          classNamePrefix="select"
+        />
+      </div>
       <h1 className="text-xl font-medium title-font m-4 text-gray-900 mb-2">{data?.length} Orte:</h1>
       {data?.map((poi) => (
         <ListElement
