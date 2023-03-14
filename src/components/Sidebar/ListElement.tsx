77e3e53b458e57552be4ab1ec23d34e5ffe9327b
Agustina <agus.carrion@icloud.com>
2023-03-14 15:24:22 +0100

    fix: add aspect ratio to image in list element

--- src/components/Sidebar/ListElement.tsx
+++ src/components/Sidebar/ListElement.tsx
@@ -18,7 +18,7 @@ const ListElement: React.FC<Props> = ({ value, hovered, ...restProps }) => {
           hovered ? 'fcmap-border-opacity-100' : ''
         } fcmap-cursor-pointer`}
       >
-        <img src={value.image} alt="POI photo" className="fcmap-w-[75px] fcmap-h-[75px]" />
+        <img src={value.image} alt={value.name} className="fcmap-h-full fcmap-aspect-square" />
         <div className="fcmap-flex-col fcmap-gap-1">
           <h1 className="fcmap-title-font fcmap-text-base fcmap-font-bold fcmap-text-gray-900">{value.name}</h1>
           <h2 className="fcmap-text-sm fcmap-title-font fcmap-font-normal fcmap-text-gray-500 fcmap-mb-1">
