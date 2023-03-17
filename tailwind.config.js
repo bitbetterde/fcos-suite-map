a810b459f2ef914a428dadae73d121be6cc7399e
Moritz Stückler <moritz@bitbetter.de>
2023-03-17 13:43:30 +0100

    fix: undo tw config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 module.exports = {
   corePlugins: {
-    preflight: true,
+    preflight: false,
   },
   content: ['./src/**/*.{html,js,jsx,tsx,md,mdx,astro}'],
   prefix: 'fcmap-',
