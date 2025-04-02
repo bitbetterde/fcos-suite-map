4978644c20c36f8ed42591e555be5ddedc9685ed
Moritz Stückler <moritz@bitbetter.de>
2025-04-02 15:36:10 +0200

    fix: use production build env

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts'
-import { devDependencies } from "./package.json";
+import dts from 'vite-plugin-dts';
+import { devDependencies } from './package.json';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/components/FabCityMap.tsx'),
-      name: 'fcos-suite-map',
+      name: '@fchh/fcos-suite-map',
       fileName: 'fcos-suite-map',
       formats: ['es'],
     },
     rollupOptions: {
-      external: [...Object.keys(devDependencies), "react", "react/jsx-runtime"],
+      external: [...Object.keys(devDependencies), 'react', 'react/jsx-runtime'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
 });
